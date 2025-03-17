2c97069f79252a36937aae1c89954d6bf96811b5
Kostiantyn Teltov <dneprokos@gmail.com>
2025-03-17 19:41:01 +0100

    Added function lessons (#2)

---
+++ 3_Functions_lessons/3_type_of_functions.py
@@ -0,0 +1,10 @@
+def greet(name):
+    print(f"Hy {name}")
+
+
+def get_greeting(name):
+    return f"Hi {name}"
+
+
+message = get_greeting("Kostia")
+print(message)
---
+++ 3_Functions_lessons/4_keyword_arguments.py
@@ -0,0 +1,7 @@
+def incement(number, by):
+    return number + by
+
+
+result = incement(2, 1)
+print(result)  # 3
+print(incement(number=2, by=1))  # 3
